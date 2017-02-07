7c5899142b089db0b582c14a4f465b433762049e
robalford <robertsalford@gmail.com>
2017-02-07 12:07:49 -0800

    Add imagekit to installed apps

--- config/settings/common.py
+++ config/settings/common.py
@@ -40,6 +40,7 @@ THIRD_PARTY_APPS = (
     'allauth',  # registration
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
+    'imagekit',  # image processing
 )
 
 # Apps specific for this project go here.
